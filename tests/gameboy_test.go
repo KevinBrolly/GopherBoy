8959a81355f606857d18e0c3444282c1ea234220
Kevin Brolly <kevin.brolly@gmail.com>
2019-03-16 22:44:40 +0000

    Ran go-lint over tests

--- tests/gameboy_test.go
+++ tests/gameboy_test.go
@@ -1,29 +1,28 @@
 package Gameboy_test
 
 import (
-    "testing"
-    "GoBoy/Gameboy"
+	"GoBoy/Gameboy"
+	"testing"
 )
 
-
 func TestP1(t *testing.T) {
-    cases := []struct {
-        Name string
-        Mode byte
-        Before  byte
-        After byte
-    }{
-        {"Mode 0", Gameboy.MODE0, 0x80, 0x80},
-        {"Mode 1", Gameboy.MODE1, 0x80, 0x81},
-        {"Mode 2", Gameboy.MODE2, 0x80, 0x82},
-        {"Mode 3", Gameboy.MODE3, 0x80, 0x83},
-    }
-    for _, tt := range cases {
-        t.Run(tt.Name, func(t *testing.T) {
-            tt.Before = Gameboy.SetBit(tt.Before, tt.pos)
-            if tt.before != tt.after {
-                t.Errorf("got %#x, want %#x", tt.before, tt.after)
-            }
-        })
-    }
+	cases := []struct {
+		Name   string
+		Mode   byte
+		Before byte
+		After  byte
+	}{
+		{"Mode 0", Gameboy.MODE0, 0x80, 0x80},
+		{"Mode 1", Gameboy.MODE1, 0x80, 0x81},
+		{"Mode 2", Gameboy.MODE2, 0x80, 0x82},
+		{"Mode 3", Gameboy.MODE3, 0x80, 0x83},
+	}
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			tt.Before = Gameboy.SetBit(tt.Before, tt.pos)
+			if tt.before != tt.after {
+				t.Errorf("got %#x, want %#x", tt.before, tt.after)
+			}
+		})
+	}
 }
